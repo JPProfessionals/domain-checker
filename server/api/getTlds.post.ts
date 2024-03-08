cdc8a7c2717125ab683e241ebde7b0afb7acd976
max <max.steinwand@gmail.com>
2024-03-08 14:57:57 +0100

    Error Body

--- server/api/getTlds.post.ts
+++ server/api/getTlds.post.ts
@@ -15,8 +15,9 @@ export default defineEventHandler(async (event) => {
     const response = await axios.get(url)
     const tlds = response.data.data.map((tld: any) => tld.name)
     return tlds // Returning the list of TLDs
-  } catch (error) {
+  } catch (error: any) {
     console.error('Error fetching TLDs from API:', error)
+    console.log('Error Body',error.response.data)
     // Returning an error response
     throw createError({ statusMessage: 'Failed to fetch TLDs', statusCode: 500 })
   }
