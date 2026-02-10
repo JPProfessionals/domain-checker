9ba699459b8cfa314af0c32e7696ab32fbad8a0a
Jan Petry <42674570+omgitsjan@users.noreply.github.com>
2026-02-10 01:41:52 +0100

    fix: remove vercel analytics code and update lockfile

--- app.vue
+++ app.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Analytics } from '@vercel/analytics/nuxt'
 import type { NavigationMenuItem } from '@nuxt/ui'
 
 const { t } = useI18n()
@@ -131,7 +130,7 @@ const availableLocales = computed(() => {
 
     <UMain>
       <NuxtPage />
-      <Analytics />
+      
     </UMain>
 
     <UFooter>
